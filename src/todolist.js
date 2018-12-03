7d48b1bcfd9e4066884c1ce8921fe314be6746a0
Will Harris <Harriswill22@yahoo.com>
2018-12-03 14:53:58 -0500

    adds form

--- src/todolist.js
+++ src/todolist.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: " ",
+            items: [],
+        }
+    }
     render() { 
         return (  
             <div>
                 <h1>
                     Todo App
                 </h1>
+
+                <form>
+                    <input placeholder="Enter Todo"></input>
+                    <button type = "submit"> Add </button>
+                </form>
             </div>
         );
     }
