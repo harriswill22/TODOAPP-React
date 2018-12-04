cc6a0ae0620453d64eb1874314729a5a6a0e593a
Will Harris <Harriswill22@yahoo.com>
2018-12-04 11:15:51 -0500

    uses find function

--- src/list.js
+++ src/list.js
@@ -3,11 +3,14 @@ import React from 'react';
 const List = (props) => {
 return (<ul>
         {props.items.map((item, index) => {
-        return <li key={index} className="single-todo" 
-        onClick={() => {
-        console.log(`clicked ${index}`);
-        props.handleClick(index);
-        }} > {item} </li>;
+        return <li key={index} 
+        className="single-todo" 
+        onClick ={props.handleClick.bind(null,index)}
+        // onClick={() => {
+        // console.log(`clicked ${index}`);
+        // props.handleClick(index);
+        // }} 
+        > {item} </li>;
         })}
         </ul>
 );
