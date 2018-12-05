3b0bdc048cd3a5adc1d4bd75613001c53731713d
Will Harris <Harriswill22@yahoo.com>
2018-12-05 09:18:18 -0500

    deletes todos based on id, not array index

--- src/list.js
+++ src/list.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const List = (props) => {
-return (<ul>
-        {props.items.map(item => {
-        return <li key={item.id} 
-        className="single-todo" 
-        // onClick ={props.handleClick.bind(null,index)}
-        onClick={() => {
-        console.log(`clicked ${item}`);
-        props.handleClick(item.id);
-        }} 
-        > {item.name} </li>;
-        })}
-        </ul>
-);
+const List = props => {
+  return (
+    <ul>
+      {props.items.map(item => {
+        console.log(item);
+        return (
+          <li 
+            key={item.id} 
+            className="single-todo"
+            onClick={() => props.handleClick(item.id)}
+          >
+            {item.name}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
-
-export default List; 
+export default List;
